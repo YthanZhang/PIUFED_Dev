5d9f87538b68d0a955fa18149b6863823a7d3dfb
Ythan Zhang <cn.yiki@gmail.com>
2021-12-30 17:41:11 +0800

    Fix bugs for simUART

--- sim_uart_test.cpp
+++ sim_uart_test.cpp
@@ -7,10 +7,13 @@
 
 #include "catch2/catch_all.hpp"
 
+#include <iostream>
+
 
 bool tx = true;
 
 uint16_t rxVal = 0b1100110101;
+uint16_t txVal = 0b1110010011;
 
 
 void setTxBit(bool txVal) { tx = txVal; }
@@ -26,46 +29,18 @@ TEST_CASE("Sim UART Test", "[sim_uart]")
     piu_SimUART_construct(&simUART, setTxBit);
     
     /** Test Tx ***************************************************************/
-    piu_SimUART_sendTx(&simUART, 0b10010011);
+    piu_SimUART_sendTx(&simUART, txVal & 0xFF);
     REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-    REQUIRE_FALSE(simUART.flag_txComplete);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(simUART.flag_txComplete);
     
     piu_SimUART_TIMUpdate(&simUART, true);
-    piu_SimUART_TIMUpdate(&simUART, true);
-    piu_SimUART_TIMUpdate(&simUART, true);
+    REQUIRE_FALSE(tx);
+
+    while(piu_SimUART_TIMUpdate(&simUART, true))
+    {
+        REQUIRE(tx == (txVal & 0x01));
+        txVal = txVal >> 1;
+    }
+    txVal = 0b1110010011;
     REQUIRE(tx);
 
     /** Test Rx ***************************************************************/
@@ -80,54 +55,23 @@ TEST_CASE("Sim UART Test", "[sim_uart]")
     REQUIRE(piu_SimUART_getRx(&simUART) == 0b00110101);
     
     /** Test simultaneous Rx Tx ***********************************************/
-    piu_SimUART_TIMUpdate(&simUART, true);
+    piu_SimUART_sendTx(&simUART, txVal & 0xFF);
     REQUIRE(tx);
-
-    piu_SimUART_sendTx(&simUART, 0b10010011);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-    REQUIRE_FALSE(simUART.flag_txComplete);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(tx);
-
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE(simUART.flag_txComplete);
     
     piu_SimUART_TIMUpdate(&simUART, true);
-    piu_SimUART_TIMUpdate(&simUART, true);
-    piu_SimUART_TIMUpdate(&simUART, true);
+    REQUIRE_FALSE(tx);
+
+    while(piu_SimUART_TIMUpdate(&simUART, true))
+    {
+        REQUIRE(tx == (txVal & 0x01));
+        txVal = txVal >> 1;
+    }
+    txVal = 0b1110010011;
     REQUIRE(tx);
     
     piu_SimUART_GPIOUpdate(&simUART, false);
     
-    while(!simUART.flag_rxComplete)
+    while(!simUART.flag_rxFrameError)
     {
         piu_SimUART_TIMUpdate(&simUART, rxVal & 0x01);
         rxVal = rxVal >> 1;
