55a0b6afca9f1e5a4b756aa9ce0fea5285925d55
Ythan Zhang <cn.yiki@gmail.com>
2022-06-09 14:34:01 +0800

    Update margined linear tests and sim uart tests

--- margined_linear_test.cpp
+++ margined_linear_test.cpp
@@ -10,40 +10,29 @@
 
 TEST_CASE("Margined linear function test", "[MarginedLinear]")
 {
-    piu_MarginedLinear ml;
+    auto ml =
+        piu_MarginedLinear_make(10, 10, 10, 50, 50, 50, 1000, 5000, 8000, 8000);
 
-    piu_MarginedLinear_construct(&ml,
-                                 10,
-                                 10,
-                                 10,
-                                 50,
-                                 50,
-                                 50,
-                                 1000,
-                                 5000,
-                                 8000,
-                                 8000);
-    
-    
-    REQUIRE(piu_MarginedLinear_input(&ml, 0) == 1000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 10) == 1000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 11) > 5000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 50) == 8000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 55) == 8000);
-    
-    piu_MarginedLinear_updatePoints(&ml, 5, 10, 20, 80, 90, 95);
-    piu_MarginedLinear_updateVal(&ml, 1000, 5000, 8000, 9000);
-    
-    REQUIRE(piu_MarginedLinear_input(&ml, 10) == 1000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 11) == 5000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 6) == 5000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 5) == 1000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 20) == 5000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 21) > 5000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 79) < 8000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 90) == 8000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 95) == 8000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 96) == 9000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 91) == 9000);
-    REQUIRE(piu_MarginedLinear_input(&ml, 90) == 8000);
+
+    REQUIRE(piu_MarginedLinear_setX(&ml, 0) == 1000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 10) == 1000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 11) > 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 50) == 8000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 55) == 8000);
+
+    piu_MarginedLinear_updateInput(&ml, 5, 10, 20, 80, 90, 95);
+    piu_MarginedLinear_updateOutput(&ml, 1000, 5000, 8000, 9000);
+
+    REQUIRE(piu_MarginedLinear_setX(&ml, 10) == 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 11) == 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 6) == 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 5) == 1000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 20) == 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 21) > 5000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 79) < 8000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 90) == 8000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 95) == 8000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 96) == 9000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 91) == 9000);
+    REQUIRE(piu_MarginedLinear_setX(&ml, 90) == 8000);
 }
--- sim_uart_test.cpp
+++ sim_uart_test.cpp
@@ -25,15 +25,15 @@ piu_SimUART simUART;
 TEST_CASE("Sim UART Test", "[sim_uart]")
 {
     piu_SimUART_construct(&simUART, setTxBit);
-    
+
     /** Test Tx ***************************************************************/
     piu_SimUART_sendTx(&simUART, txVal & 0xFF);
     REQUIRE(tx);
-    
-    piu_SimUART_TIMUpdate(&simUART, true);
+
+    piu_SimUART_halfDuplexTIMUpdate(&simUART, true);
     REQUIRE(!tx);
 
-    while(piu_SimUART_TIMUpdate(&simUART, true))
+    while (piu_SimUART_halfDuplexTIMUpdate(&simUART, true))
     {
         REQUIRE(tx == (txVal & 0x01));
         txVal = txVal >> 1;
@@ -43,38 +43,73 @@ TEST_CASE("Sim UART Test", "[sim_uart]")
 
     /** Test Rx ***************************************************************/
     piu_SimUART_GPIOUpdate(&simUART, false);
-    
-    while(!simUART.flag_rxComplete)
+
+    while (!simUART.flag_rxComplete)
     {
-        piu_SimUART_TIMUpdate(&simUART, rxVal & 0x01);
+        piu_SimUART_halfDuplexTIMUpdate(&simUART, rxVal & 0x01);
         rxVal = rxVal >> 1;
     }
-    
+
     REQUIRE(piu_SimUART_getRx(&simUART) == 0xF0);
-    
-    /** Test simultaneous Rx Tx ***********************************************/
+
+    /** Test simultaneous Rx Tx on half duplex ********************************/
+    piu_SimUART_GPIOUpdate(&simUART, false);
+    rxVal = 0b1101010101;
+
+    // tx should be pending be not started
     piu_SimUART_sendTx(&simUART, txVal & 0xFF);
     REQUIRE(tx);
-    
-    piu_SimUART_TIMUpdate(&simUART, true);
-    REQUIRE_FALSE(tx);
 
-    while(piu_SimUART_TIMUpdate(&simUART, true))
+    while (!piu_SimUART_getRxComplete(&simUART))
+    {
+        piu_SimUART_halfDuplexTIMUpdate(&simUART, rxVal & 0x01);
+        rxVal = rxVal >> 1;
+
+        REQUIRE(tx);    // tx should not change
+    }
+
+    piu_SimUART_halfDuplexTIMUpdate(&simUART, false);
+    REQUIRE(tx == 0);
+
+    while (piu_SimUART_halfDuplexTIMUpdate(&simUART, false))
     {
         REQUIRE(tx == (txVal & 0x01));
         txVal = txVal >> 1;
     }
-    txVal = 0b1110010011;
-    REQUIRE(tx);
-    
-    piu_SimUART_GPIOUpdate(&simUART, false);
-    
+
+    REQUIRE(piu_SimUART_getRx(&simUART) == 0b01010101);
+
+    /** Test simultaneous Rx Tx on full duplex ********************************/
     rxVal = 0b1101010101;
-    while(!simUART.flag_rxComplete)
+    txVal = 0b1110010011;
+    piu_SimUART_GPIOUpdate(&simUART, false);
+    piu_SimUART_sendTx(&simUART, txVal & 0xFF);
+
+    piu_SimUART_txTIMUpdate(&simUART);
+    REQUIRE_FALSE(tx);
+
+    int i = 0;
+    while (!piu_SimUART_getRxComplete(&simUART))
     {
-        piu_SimUART_TIMUpdate(&simUART, rxVal & 0x01);
+        piu_SimUART_rxTIMUpdate(&simUART, rxVal & 0x01);
         rxVal = rxVal >> 1;
+
+        ++i;
+        if (i % 2)
+        {
+            piu_SimUART_txTIMUpdate(&simUART);
+            REQUIRE(tx == (txVal & 0x01));
+            txVal >>= 1;
+        }
+    }
+
+    while (!piu_SimUART_getTxComplete(&simUART))
+    {
+        piu_SimUART_txTIMUpdate(&simUART);
+        REQUIRE(tx == (txVal & 0x01));
+        txVal >>= 1;
     }
     
-    REQUIRE(piu_SimUART_getRx(&simUART) == 0b01010101);
+    rxVal = 0b1101010101;
+    REQUIRE(piu_SimUART_getRx(&simUART) == (rxVal & 0xFF));
 }
